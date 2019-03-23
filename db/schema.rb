49b73bae02a8eb69c07685f825931ae795a49fbc
Pedro Schmitt <pedromartins.bsb@gmail.com>
2019-03-23 16:28:55 -0300

    Bootstrap views for devise

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2019_03_23_183025) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
